42c425783a761baad33132b4b46b1de5315a55ce
Hajjar Med Chedly <chhajjar2006@gmail.com>
2021-07-26 00:36:13 +0100

    test jsonb

---
+++ src/main/kotlin/org/chedly/Greeting.kt
@@ -0,0 +1,3 @@
+package org.chedly
+
+data class Greeting(val message: String = "")
--- src/main/kotlin/org/chedly/GreetingResource.kt
+++ src/main/kotlin/org/chedly/GreetingResource.kt
@@ -3,12 +3,20 @@ package org.chedly
 import javax.ws.rs.GET
 import javax.ws.rs.Path
 import javax.ws.rs.Produces
-import javax.ws.rs.core.MediaType
+
+import javax.ws.rs.core.MediaType;
+import org.chedly.Greeting;
 
 @Path("/hello")
 class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello RESTEasy"
+    fun hello() = Greeting("hello")
+
+
+    @GET
+    @Path("/pojo")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun helloe() = org.chedly.Greeting("hello")
 }
